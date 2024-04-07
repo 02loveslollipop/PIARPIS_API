73858f3594b22ac038faa078994873ea56a19e88
02loveslollipop <mmonsalve2005@gmail.com>
2024-04-07 02:00:41 -0500

    Updated api and addded mongo_tester

--- api.py
+++ api.py
@@ -1,12 +1,11 @@
 #flask api
 import requests
 from flask import Flask, request, jsonify
+from mongo import MongoClient
 import json
 from config import config
-from request import ModelRequest
 from functools import wraps
 from datetime import datetime as dt
-from pymongo import MongoClient
 from flask_cors import CORS
 import hashlib
 import pandas as pd
@@ -15,18 +14,17 @@ api = Flask(__name__)
 cors = CORS(api)
 api.config['CORS_HEADERS'] = 'Content-Type'
 conf = config()
-iaRequest = ModelRequest(host=conf.ia_ip,port=conf.ia_port,argsList=['ingredients'],resource='request')
-db = MongoClient()
+
 session = []
 
 def connect_to_database():
     client = MongoClient(conf.mongo_uri)
-    return client[conf.mongo_db]
+    return client['db']
 
 def login_required(func): # Wrapper to check if the user is in session if required
     @wraps(func)
     def wrapper(*args, **kwargs):
-        username_hash = request.headers.get('secretAuth')
+        username_hash = request.headers.get('hash')
         if username_hash not in session:
             return jsonify({'message': 'user not in session'}), 401
         return func(*args, **kwargs)
@@ -46,14 +44,22 @@ def checkPassword(username,password) -> bool: # check if the password is correct
     else:
         return False
 
-@api.route('/login', methods=['GET'])
+@api.route('/login', methods=['POST'])
 def login(): #Add user to session
-    username = request.headers.get('username')
-    password = request.headers.get('password')
+    username = request.json.get('username')
+    password = request.json.get('password')
     
     if checkPassword(username,password):
         username_hash = hashlib.sha256((username + dt.now.__str__()).encode()).hexdigest()
         session.append(username_hash)
+        #curl to logout the user
+        print("--------CURL COMMANDS--------")
+        print( f"curl -X GET http://localhost:6970/logout -H 'secret:{username_hash}'\n\n")
+        #curl to insert to db
+        print( f"curl -X POST http://localhost:6970/insert_to_db -H 'Content-Type: application/json' -H 'hash: {username_hash}' -d '{{\"Id\": \"1\", \"name\": \"test\", \"plate\": \"test\", \"invoice\": \"test\", \"inicial_time\": \"test\"}}'\n\n")
+        #curl to get parkings
+        print( f"curl -X GET http://localhost:6970/get_parkings_db -H 'hash: {username_hash}'\n\n")
+        print("--------CURL COMMANDS--------")
         return jsonify({'secretAuth': username_hash}), 200
     else:
         return jsonify({'message': 'login failed'}), 401
@@ -65,52 +71,42 @@ def logout(): #Remove user from session
     session.pop(username_hash)
     return jsonify({'message': 'logout successful'}), 200
 
-@api.route('/create_recipe', methods=['GET'])
+@api.route('/insert_to_db', methods=['POST'])
 @login_required
-def createRecipe(): # request a recipe inference to the IA model
-    ingredients = request.headers.get('ingredients') #The ingredinetes are passed as a vector of strings
-    response = iaRequest.request([ingredients]) #Send the request to the IA model
-    if response.status_code == 200: #If the request was successful
-        return response.json(), 200
-    else:
-        return jsonify({'message': "This didn't work as expected :("}), 500 #TODO: search a less silly message
+def addToDB():
+    username = request.json.get('Id')
+    name = request.json.get('name')
+    plate = request.json.get('plate')
+    invoice = request.json.get('invoice')
+    inicial_time = request.json.get('inicial_time')
 
-@api.route('/insert_recipe_db', methods=['GET'])
-@login_required
-def addToFavoriteDB():
-    username = request.headers.get('username')
-    name = request.headers.get('name')
-    url = request.headers.get('url')
-    print(username)
-    print(name)
-    print(url)
     database = connect_to_database()
-    users_collection = database['Usr']
-    user = users_collection.find_one({'name': username})
-    if not user:
-        return jsonify({'message': 'user not found'}), 401
+    parking_collection = database['Parkings']
+    parking_collection.insert_one({'Id': username, 'name': name, 'plate': plate, 'invoice': invoice, 'inicial_time': inicial_time})
+    return jsonify({'message': 'added to parking register'}), 200 #TODO: search a less silly message
 
-    users_collection.update_one({'name': username}, {'$push': {'likedRecipes': {'recipe_name': name, 'recipe_urls': url}}})
-    
-    return jsonify({'message': 'added to favorite recipes of the user :)'}), 200 #TODO: search a less silly message
-
-@api.route('/get_recipe_db', methods=['GET'])
+@api.route('/get_parkings_db', methods=['GET'])
 @login_required
-def getRecipeDB(): # get all the recipes of the user
+def getParkingDB(): # get all the recipes of the user
     username = request.headers.get('user')
-    
-    #the collection have an array of objects (that are the recipes) that is called likedRecipes, the objects have 2 fields: recipe_name and recipe_urls, when called this function, the function will extract the array of objects and return it as a json
 
     database = connect_to_database()
-    users_collection = database['Usr']
-    recipes = users_collection.find_one({'name': username})
-    if not recipes:
-        return jsonify({'message': 'user not found'}), 401
+    parking_collection = database['Parkings']
+    parkings = parking_collection.data
+    if len(parkings) == 0:
+        return jsonify({'message': 'No parkings found'}), 404
     
-    response = recipes['likedRecipes']
-    response.pop(0)
-    responseDataframe = pd.DataFrame(response).to_json(orient='records')
-    return responseDataframe, 200   
-
+    response = jsonify(parkings)
+    return response, 200 
 if __name__ == '__main__':
     api.run(debug=True, host='0.0.0.0', port=6970)
+    
+'''
+curl -X POST http://localhost:6970/login -H "Content-Type: application/json" -d '{"username": "admin", "password": "admin"}'
+
+curl -X GET http://localhost:6970/logout -H "hash:<replace with the hash from the login response>" #printed in the console when the user logs in
+
+curl -X POST http://localhost:6970/insert_to_db -H "Content-Type: application/json, hash: <replace with the hash from the login response>" -d '{"Id": "1", "name": "test", "plate": "test", "invoice": "test", "inicial_time": "test"}'
+
+curl -X GET http://localhost:6970/get_parkings_db -H "hash: <replace with the hash from the login response>"
+'''
